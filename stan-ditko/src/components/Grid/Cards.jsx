dbfa3a22cd87edbe7956ed0f59409b747fe44707
Augusto Carmona Perez <augustocarmonaperez@gmail.com>
2021-05-30 19:18:36 -0300

    fix: changed html structure for map method in cards

---
+++ stan-ditko/src/components/Grid/Cards.jsx
@@ -0,0 +1,50 @@
+export const CardsInfo = [
+    {
+        name: 'Wolverine',
+        img_url: 'https://i.pinimg.com/originals/87/dc/02/87dc02961d6c4afaf0fd458cf4f3f261.jpg',
+        cName: 'grid-card',
+        url: '#'
+    },
+    {
+        name: 'Scarlet Witch',
+        img_url: 'https://otakukart.com/wp-content/uploads/2021/05/Marvel-Characters-Scarlet-Witch.jpg',
+        cName: 'grid-card',
+        url: '#'
+    },
+    {
+        name: 'Spider Man',
+        img_url: 'https://dam.smashmexico.com.mx/wp-content/uploads/2018/09/07142333/spidermanbio_portada.jpg',
+        cName: 'grid-card',
+        url: '#'
+    },
+    {
+        name: 'Hulk',
+        img_url: 'https://wallpaperaccess.com/full/3954030.jpg',
+        cName: 'grid-card',
+        url: '#'
+    },
+    {
+        name: 'Capitan America',
+        img_url: 'https://i.pinimg.com/originals/13/8a/b1/138ab1588ee061065f626c726705a39b.jpg',
+        cName: 'grid-card',
+        url: '#'
+    },
+    {
+        name: 'Dr. Strange',
+        img_url: 'https://i.pinimg.com/736x/8c/3a/03/8c3a0385d43aea79e3d182f0da1a5d19.jpg',
+        cName: 'grid-card',
+        url: '#'
+    },
+    {
+        name: 'Black Widow',
+        img_url: 'https://static1.srcdn.com/wordpress/wp-content/uploads/2020/01/Black-Widow-Widows-Sting-Marvel-Comic.jpg',
+        cName: 'grid-card',
+        url: '#'
+    },
+    {
+        name: 'Iron Man',
+        img_url: 'https://i.pinimg.com/originals/5f/ec/01/5fec01fcc234324095cab20cd43dbb6a.jpg',
+        cName: 'grid-card',
+        url: '#'
+    }
+]
--- stan-ditko/src/components/Grid/Grid.jsx
+++ stan-ditko/src/components/Grid/Grid.jsx
@@ -1,83 +1,23 @@
 import React, { Component } from 'react';
 import './Grid.css';
+import { CardsInfo } from './Cards.jsx';
 
 class Grid extends Component {
     render() {
         return (
             <section className="grid">
                 <div className="grid_container">
-
-                    <div className="grid-card">
-                        <a href="">
-                            <img src="https://i.pinimg.com/originals/87/dc/02/87dc02961d6c4afaf0fd458cf4f3f261.jpg" alt="Wolverine" className="grid-card_img"/>
-                            <div className="grid-card_details">
-                                <p>Wolverine</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className="grid-card">
-                        <a href="">
-                            <img src="https://otakukart.com/wp-content/uploads/2021/05/Marvel-Characters-Scarlet-Witch.jpg" alt="Scarlet Witch" className="grid-card_img"/>
-                            <div className="grid-card_details">
-                                <p>Scarlet Witch</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className="grid-card">
-                        <a href="">
-                            <img src="https://dam.smashmexico.com.mx/wp-content/uploads/2018/09/07142333/spidermanbio_portada.jpg" alt="Spider Man" className="grid-card_img"/>
-                            <div className="grid-card_details">
-                                <p>Spider Man</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className="grid-card">
-                        <a href="">
-                            <img src="https://wallpaperaccess.com/full/3954030.jpg" alt="Hulk" className="grid-card_img"/>
-                            <div className="grid-card_details">
-                                <p>Hulk</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className="grid-card">
-                        <a href="">
-                            <img src="https://i.pinimg.com/originals/13/8a/b1/138ab1588ee061065f626c726705a39b.jpg" alt="Capitan America" className="grid-card_img"/>
-                            <div className="grid-card_details">
-                                <p>Capitan America</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className="grid-card">
-                        <a href="">
-                            <img src="https://i.pinimg.com/736x/8c/3a/03/8c3a0385d43aea79e3d182f0da1a5d19.jpg" alt="Dr. Strange" className="grid-card_img"/>
-                            <div className="grid-card_details">
-                                <p>Dr. Strange</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className="grid-card">
-                        <a href="">
-                            <img src="https://i.pinimg.com/736x/c9/d7/32/c9d7328eaef2bba6e93c313cac0be8e0.jpg" alt="Black Widow" className="grid-card_img"/>
-                            <div className="grid-card_details">
-                                <p>Black Widow</p>
-                            </div>
-                        </a>
-                    </div>
-
-                    <div className="grid-card">
-                        <a href="">
-                            <img src="https://i.pinimg.com/originals/5f/ec/01/5fec01fcc234324095cab20cd43dbb6a.jpg" alt="Iron Man" className="grid-card_img"/>
-                            <div className="grid-card_details">
-                                <p>Iron Man</p>
-                            </div>
-                        </a>
-                    </div>
+                    {CardsInfo.map((item, index)=>{
+                        return (
+                        <div className={item.cName} key={index}>
+                            <a href={item.url}>
+                                <img src={item.img_url} alt={item.name} className="grid-card_img"/>
+                                <div className="grid-card_details">
+                                    <p>{item.name}</p>
+                                </div>
+                            </a>
+                        </div>);
+                    })}
                 </div>
             </section>
         );
