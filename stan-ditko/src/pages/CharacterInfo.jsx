607ea7df4357617045530057b19ee1ceeb311129
Augusto Carmona Perez <augustocarmonaperez@gmail.com>
2021-06-12 14:12:36 -0300

    fix: components renamed

--- stan-ditko/src/pages/CharacterInfo.jsx
+++ stan-ditko/src/pages/CharacterInfo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../assets/css/Normalize.css';
 import '../assets/css/App.css';
 import SearchBar from '../components/SearchBar/SearchBar';
-import HeroDetails from '../components/HeroDetails/HeroDetails';
+import HeroDetails from '../components/CharacterInfo/CharacterInfo';
 import ComicsList from '../components/ComicsList/ComicsList';
 import Footer from '../components/Footer/Footer';
 
